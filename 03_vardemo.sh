c0029bd20e4cbb64d8bdbdcc41f6c43824be99c5
Ubuntu <ubuntu@ip-172-31-36-148.eu-north-1.compute.internal>
2026-03-19 08:44:38 +0000

    variable demo script added

---
+++ 03_vardemo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Script to show how to use variables
+
+name="naveen"
+age=23
+error = 0
+
+echo "my name is $name and my age is $age"
+
+name="chhetri"
+
+echo "my name is $name"
+
+#Var to store a output of a command
+HOSTNAME=$(hostname)
+
+echo "name of this machine is $HOSTNAME"
+
