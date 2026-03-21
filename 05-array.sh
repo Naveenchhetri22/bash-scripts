63871cdb461c4f508b408ac9f9f66d36288badbb
Ubuntu <ubuntu@ip-172-31-36-148.eu-north-1.compute.internal>
2026-03-21 09:07:03 +0000

    demo array file updated

--- 05-array.sh
+++ 05-array.sh
@@ -9,3 +9,17 @@ myarray=(1 2 35.6 hello "hello world! i am naveen chhetri")
 echo "${myarray[*]}" #getting all values from array
 
 echo "${myarray[4]}" #getting value through index number
+
+#how to find no. of values in array
+
+echo "no of values, length of an array is ${#myarray[*]}"
+
+#get value in range in array
+
+echo "values from index 2-3 is: ${myarray[*]:2:2}"
+
+#adding new values inside previous array
+
+myarray+=( NEW 30 50 )
+
+echo "here last three new values: ${myarray[*]}"
