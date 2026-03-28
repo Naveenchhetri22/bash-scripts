1311b49144d6ca01ce0c460cf6c7f7c71e4bfc49
Ubuntu <ubuntu@ip-172-31-36-148.eu-north-1.compute.internal>
2026-03-28 10:01:10 +0000

    static funtion demo file added

---
+++ 23_function.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#demo of function
+
+function myfun {
+	echo "YO! here is function demo..........."
+	echo "----------------------------------------"
+}
+
+myfun
+myfun
+myfun
+myfun
+
