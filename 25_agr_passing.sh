249417a25b52b958e75caa057914a8bf288f5ad9
Ubuntu <ubuntu@ip-172-31-36-148.eu-north-1.compute.internal>
2026-03-31 12:31:41 +0000

    passing an arguments from direct command line

---
+++ 25_agr_passing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#for single argumment and seperate values
+
+echo "First argument is: $1"
+echo "Second argument is: $2"
+echo "Third argument is: $3"
+
+#for all arguments
+
+echo "All arguments: $@"
+
+#for number of arguments only
+
+echo "Number of arguments are: $#"
